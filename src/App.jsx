956a399ca31c308374fe4b7f0f8e2351afa5e1ba
GLB25114CN213 <rishabhmishra2537@gmail.com>
2026-06-17 00:55:56 +0530

    RENDER

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ export default function MedIntelAI() {
     formData.append("file", reports[0].file);
 
     const response = await fetch(
-  "https://medintel-backend-o74v.onrender.com/analyze",
+  "https://medintel-9pak.onrender.com/analyze",
   {
     method: "POST",
     body: formData,
